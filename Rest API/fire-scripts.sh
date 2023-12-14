2974fcb175c5141b72fa4a39321fe456cb17ac47
Bruno Bernardi <bruno.bernardi@suse.com>
2023-12-14 11:15:31 -0300

    Update fire-scripts.sh

--- Rest API/fire-scripts.sh
+++ Rest API/fire-scripts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#This script is responsible for calling each of the api-rest scripts simultaneously and executing the processes in the background via PID, increasing the load on NeuVector and not depending on an open user session.
+#This script calls each API-rest script simultaneously and executes the processes in the background via PID, increasing the load on NeuVector and not depending on an open user session.
 
 #Create a file with a defined number for API requests
-for ((i=1;i<=250;i++)); do ; echo $i; done > api-loop.txt
+for ((i=1;i<=250;i++)); do echo $i; done > api-loop.txt
 sleep 5
 
 nohup ./api-rest.sh &
