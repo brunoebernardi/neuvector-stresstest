82538b58761eb867def6275c03d6858e65857878
Bruno Bernardi <bruno.bernardi@suse.com>
2023-12-14 11:02:02 -0300

    Fix syntax on request.sh

--- Pods & Namespaces/request.sh
+++ Pods & Namespaces/request.sh
@@ -1,7 +1,10 @@
 #Generate 250 requests in each of the pods and namespaces created.
 #This will increase the load on the nodes of the kubernetes cluster and also on the enforcers.
 
-DATE=$(date +%Y-%m-%d-%H:%M)
+DATE=$(date +%Y-%m-%d)
+
+#Create a file with a defined number for namespaces and requests
+for ((i=1;i<=250;i++)); do echo $i; done > loop-namespaces.txt
 
 for i in $(cat loop-namespaces.txt)
 do
